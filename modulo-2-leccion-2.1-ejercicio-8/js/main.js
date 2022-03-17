6bc9115f8920d3fa78287db91573166cd5c61cc4
Analía Río <analia.rio@gmail.com>
2022-03-17 23:07:20 +0100

    Yet even more JS exercises

--- modulo-2-leccion-2.1-ejercicio-8/js/main.js
+++ modulo-2-leccion-2.1-ejercicio-8/js/main.js
@@ -5,7 +5,7 @@ const btn2Element = document.querySelector(".btn2");
 
 // Declaramos la clase que queremos añadir
 
-btn2.classList.add("disable");
+btn2Element.classList.add("disable");
 
 
 // Crear dos botones sencillos con los mismos estilos (padding, borde, color) y, usando JavaScript, al segundo añadirle una clase para que parezca que está desactivado (por ejemplo, aplicarle una opacidad menor)
