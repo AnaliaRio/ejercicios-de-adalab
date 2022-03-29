dfdc5abeaf327c71289be15558e1c0ab1ca00b2e
Analía Río <analia.rio@gmail.com>
2022-03-29 23:22:16 +0200

    Update

--- modulo-2-leccion-8.2-ejercicio-1-centrar-careto/js/main.js
+++ modulo-2-leccion-8.2-ejercicio-1-centrar-careto/js/main.js
@@ -1,2 +1,17 @@
 "use strict";
 
+// const face = document.querySelector(".js-face");
+
+
+// function smiley (face) {
+//     const face = document.querySelector(".js-face");
+//     if (face.innerHTML === ":)") {
+//         face.innerHTML = ";)";
+//     }
+//     else {
+//         face.innerHTML = ":)";
+//     }
+// }
+
+// face.addEventListener("click", smiley);
+
